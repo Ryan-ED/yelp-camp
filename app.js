07f57d57ed1015607109842a118ba89b31a95b54
Ryan D <ryan.aka.spike@gmail.com>
2017-12-31 14:49:56 +0200

    Installed body-parser package. Added a POST route and view for adding a new camp.

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
     res.render("landing");
 });
 
+var camps = [
+    {name: "Wye Valley Camping", img: "http://www.photosforclass.com/download/7121861565"},
+    {name: "Granite Hill", img: "http://www.photosforclass.com/download/1342367857"},    
+    {name: "Jenny Lake Camp", img: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"}  
+];
+
 app.get('/campgrounds', function(req, res) {
-    var camps = [
-        {name: "Wye Valley Camping", img: "http://www.photosforclass.com/download/7121861565"},
-        {name: "Granite Hill", img: "http://www.photosforclass.com/download/1342367857"},    
-        {name: "Jenny Lake Camp", img: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"}  
-    ];
     res.render("campgrounds", {camps: camps});
+});
+
+app.post('/campgrounds', function(req, res) {
+    var name = req.body.name;
+    var img = req.body.img;
+    camps.push({ name: name, img: img });
+    res.redirect("/campgrounds");
+});
+
+app.get('/campgrounds/new', function(req, res) {
+    res.render("new.ejs");
 });
 
 app.listen(3000, function() {
