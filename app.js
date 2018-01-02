2f71a8d5c88066b6225ead3539753fd3b523ce33
Ryan D <ryan.aka.spike@gmail.com>
2018-01-02 14:22:41 +0200

    Added data persistence with mongoDB. New campgrounds are now permanently saved even after the server restarts. Required mongo, opened a connection, created a schema and then a model using that schema. Updated the POST method to use the model instead of the hard-coded array. Also deleted the array and changed variable names for consistency

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+var campSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campSchema);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,27 +19,32 @@ app.get('/', function(req, res) {
     res.render("landing");
 });
 
-var camps = [
-    {name: "Wye Valley Camping", img: "http://www.photosforclass.com/download/7121861565"},
-    {name: "Granite Hill", img: "http://www.photosforclass.com/download/1342367857"},    
-    {name: "Jenny Lake Camp", img: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},  
-    {name: "Wye Valley Camping", img: "http://www.photosforclass.com/download/7121861565"},
-    {name: "Granite Hill", img: "http://www.photosforclass.com/download/1342367857"},    
-    {name: "Jenny Lake Camp", img: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
-    {name: "Wye Valley Camping", img: "http://www.photosforclass.com/download/7121861565"},
-    {name: "Granite Hill", img: "http://www.photosforclass.com/download/1342367857"},    
-    {name: "Jenny Lake Camp", img: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"}
-];
-
 app.get('/campgrounds', function(req, res) {
-    res.render("campgrounds", {camps: camps});
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, camps){
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds", {camps: camps});
+        }
+    });
 });
 
 app.post('/campgrounds', function(req, res) {
     var name = req.body.name;
-    var img = req.body.img;
-    camps.push({ name: name, img: img });
-    res.redirect("/campgrounds");
+    var image = req.body.image;
+    Campground.create({
+        name: name, 
+        image: image
+    }, function(err, camp){
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get('/campgrounds/new', function(req, res) {
